0b1a50f57ca3d3e124e4a50ac04644a99c0be59e
Sebastian Pallero <sebastianoria@hotmail.com>
2023-01-22 17:54:15 -0300

    [delete] popularCategory folder

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,8 +1,7 @@
-export {default as PopularCategory} from './popularCategory/index';
 export {default as Main} from './main/index';
 export {default as Products} from './products/index';
 export {default as Product} from './product/index';
 export {default as Cart} from './cart/index';
 export {default as Orders} from './orders/index';
-export {default as Auth} from './auth/index'
-export {default as Maps} from './maps/index'
+export {default as Auth} from './auth/index';
+
